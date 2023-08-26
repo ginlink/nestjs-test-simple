3df07672993a912c23d98bc6e5b51921584a691e
ginlink <ginlink1998@gmail.com>
2023-08-26 16:33:42 +0800

    feat: add user service and controller test

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -1,14 +1,24 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { User } from './entities/user.entity';
 
 describe('UsersController', () => {
   let controller: UsersController;
 
+  const mockUserService = {
+    create: jest.fn(),
+    findAll: jest.fn(),
+    findOne: jest.fn(),
+    update: jest.fn(),
+    remove: jest.fn(),
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
-      providers: [UsersService],
+      providers: [{ provide: UsersService, useValue: mockUserService }],
     }).compile();
 
     controller = module.get<UsersController>(UsersController);
@@ -17,4 +27,32 @@ describe('UsersController', () => {
   it('should be defined', () => {
     expect(controller).toBeDefined();
   });
+
+  it('create => should create a user and return its data', async () => {
+    // arrange
+    const createUserDto: CreateUserDto = {
+      firstname: 'John',
+      lastname: 'Smith',
+      email: '1@gmail.com',
+    };
+    const user: User = {
+      id: Date.now(),
+      firstname: createUserDto.firstname,
+      lastname: createUserDto.lastname,
+      email: createUserDto.email,
+    };
+    jest.spyOn(mockUserService, 'create').mockReturnValue(user);
+
+    // act
+    const result = await controller.create(createUserDto);
+
+    // assert
+    expect(result).toEqual(user);
+    expect(mockUserService.create).toBeCalled();
+    expect(mockUserService.create).toBeCalledWith(createUserDto);
+  });
+  it('findAll => should return an array of users', () => {});
+  it('findOne => should find a user by a given id and return its data', () => {});
+  it('update => should find a user by a given id and update its data', () => {});
+  it('remove => should find a user by a given id, remove and then return Number of affected rows', () => {});
 });
--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -1,12 +1,28 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersService } from './users.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { CreateUserDto } from './dto/create-user.dto';
+
+const mockUserRepository = {
+  save: jest.fn(),
+  find: jest.fn(),
+  findOne: jest.fn(),
+  delete: jest.fn(),
+};
 
 describe('UsersService', () => {
   let service: UsersService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
+      providers: [
+        UsersService,
+        {
+          provide: getRepositoryToken(User),
+          useValue: mockUserRepository,
+        },
+      ],
     }).compile();
 
     service = module.get<UsersService>(UsersService);
@@ -14,5 +30,88 @@ describe('UsersService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('create => Should create a new user and return its data', async () => {
+    // arrange
+    const createUserDto: CreateUserDto = {
+      firstname: 'John',
+      lastname: 'Smith',
+      email: '1@gmail.com',
+    };
+
+    const user = {
+      id: Date.now(),
+      firstname: createUserDto.firstname,
+      lastname: createUserDto.lastname,
+      email: createUserDto.email,
+    };
+
+    jest.spyOn(mockUserRepository, 'save').mockReturnValue(user);
+
+    // act
+    const result = await service.create(createUserDto);
+
+    // assert
+    expect(mockUserRepository.save).toBeCalled();
+    expect(mockUserRepository.save).toBeCalledWith(createUserDto);
+    expect(result).toEqual(user);
+  });
+  it('findAll => Should return an array of users', async () => {
+    // arrange
+    const user: User = {
+      id: Date.now(),
+      firstname: 'John',
+      lastname: 'Smith',
+      email: '1@gmail.com',
+    };
+    const users = [user];
+    jest.spyOn(mockUserRepository, 'find').mockReturnValue(users);
+
+    // act
+    const result = await service.findAll();
+
+    // assert
+    expect(mockUserRepository.find).toBeCalled();
+    expect(result).toEqual(users);
+  });
+  it('findOne => should find a user by a given id and return its data', async () => {
+    // arrange
+    const id = 1;
+    const user: User = {
+      id,
+      firstname: 'John',
+      lastname: 'Smith',
+      email: '1@gmail.com',
+    };
+    jest.spyOn(mockUserRepository, 'findOne').mockReturnValue(user);
+
+    // act
+    const result = await service.findOne(id);
+
+    // assert
+    expect(result).toBe(user);
+    expect(mockUserRepository.findOne).toBeCalled();
+    expect(mockUserRepository.findOne).toBeCalledWith({ where: { id } });
+  });
+  // it('update', () => {});
+  it('remove => should find a user by a given id, remove and then return Number of affected rows', async () => {
+    // arrange
+    const id = 1;
+    const user: User = {
+      id,
+      firstname: 'John',
+      lastname: 'Smith',
+      email: '1@gmail.com',
+    };
+    jest.spyOn(mockUserRepository, 'delete').mockReturnValue(user);
+
+    // act
+    const result = await service.remove(id);
+
+    // assert
+    expect(result).toEqual(user);
+    expect(mockUserRepository.delete).toBeCalled();
+    expect(mockUserRepository.delete).toBeCalledWith(id);
   });
 });
